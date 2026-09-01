fd2b7ee82f2a8059d3886c10799f36c3ff50b905
Michael-Kr26 <73739882+Michael-Kr26@users.noreply.github.com>
2026-09-01 14:40:35 +0200

    R5: houd serverstart compact

---
+++ start-server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const originalLog = console.log.bind(console);
+
+console.log = (...args) => {
+    const first = String(args[0] ?? '');
+    if (first.startsWith('Database verbonden:')) return;
+    originalLog(...args);
+};
+
+require('./roster-planner-bootstrap');
